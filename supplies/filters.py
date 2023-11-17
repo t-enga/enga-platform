ecb858e283b20609d3a814bbd5b0ff4ec27db98f
t-enga <cristian.ortega@transportesenga.com>
2023-11-17 11:40:48 -0600

    update 7

--- supplies/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from .models import SupplieStatus
-
-class SupplieFilter(django_filters.FilterSet):
-    class Meta:
-        model = SupplieStatus
-        fields = ('status', 'unit', 'driver', 'turn', 'supplier','condition' )  # campos a filtrar
